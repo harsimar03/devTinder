2d5db532c58bee536a55f902e15eec6d83ccb46d
harsimar-singh03 <23ume523@lnmiit.ac.in>
2025-04-11 18:16:08 +0530

    get/post functionality

--- app.js
+++ app.js
@@ -2,23 +2,31 @@ const express= require("express");
 
 const app= express();
 
-
-
-app.use("/game",(req,res)=>{
-    res.send("hello gamer")
+app.get("/user",(req,res)=>{
+    res.send({name:"harsimar",age:23})
 })
 
-app.use("/exam",(req,res)=>{
-    res.send("hello exam")
+app.post("/user",(req,res)=>{
+    res.send("data posted ")
 })
 
-app.use("/test",(req,res)=>{
-    res.send("hello harsimar")
-})
 
-app.use("/",(req,res)=>{
-    res.send("hello singh")
-})
+
+// app.use("/game",(req,res)=>{
+//     res.send("hello gamer")
+// })
+
+// app.use("/exam",(req,res)=>{
+//     res.send("hello exam")
+// })
+
+// app.use("/test",(req,res)=>{
+//     res.send("hello harsimar")
+// })
+
+// app.use("/",(req,res)=>{
+//     res.send("hello singh")
+// })
 
 app.listen(3000,()=>{
     console.log("server is created at 3000 ..")
